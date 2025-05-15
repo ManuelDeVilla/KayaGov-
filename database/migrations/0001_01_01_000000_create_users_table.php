a06193e9705dc114625b5d9536f48cb273876f18
ManuelDeVilla <manueldevilla25@gmail.com>
2025-05-15 10:17:38 +0800

    updated users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->string('usertype');
+            $table->string('image_path');
             $table->timestamps();
         });
 

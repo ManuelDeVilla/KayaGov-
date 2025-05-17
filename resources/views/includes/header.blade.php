fe55f549bda4e8ce231898ef502f3b2a9eddb388
Anna <dagosgc@students.nu-laguna.edu.ph>
2025-05-17 16:23:57 +0800

    Add header component with logo and user profile

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,15 @@
+
+<link rel="stylesheet" href="{{ asset('css/header.css') }}">
+<div class="main-header">
+    <div class="logo">
+        <a href="{{ route('homepage') }}">
+            <img src="{{ asset('images/kayagov_logo.png') }}" alt="KayaGov Logo" ">
+        </a>
+    </div>
+    <nav class="nav-links">
+        <div class="user-profile">
+            <img src="{{ asset('images/circle-user-round.svg') }}" alt="User Icon" class="user-icon">
+            <span class="user-name">Anna</span>
+        </div>
+    </nav>
+</div>

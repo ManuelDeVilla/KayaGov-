0d4a4a8f0d7b39f7978c45674f75b66e4fd7b6f6
Anna <dagosgc@students.nu-laguna.edu.ph>
2025-05-17 16:25:36 +0800

    Remove unused header section from navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -4,11 +4,3 @@
     <a href="{{ route('show.register') }}">Register</a>
     <a href="">Logout</a>
 </nav>
-
-<link rel="stylesheet" href="{{ asset('css/header.css') }}">
-<div class="main-header">
-    <div class="logo" href="{{ route('homepage') }}">KayaGov</div>
-    <nav class="nav-links">
-        <a href="#">Logout</a>
-    </nav>
-</div>

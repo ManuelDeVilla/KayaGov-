27671eea54cf9b0f6acb610631e6f5d3d30508d9
Anna <dagosgc@students.nu-laguna.edu.ph>
2025-05-17 16:25:21 +0800

    just renamed Homepage to Home :)

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,6 +1,14 @@
 <nav>
-    <a href="{{ route('homepage') }}">Homepage</a>
+    <a href="{{ route('homepage') }}">Home</a>
     <a href="{{ route('show.login') }}">Login</a>
     <a href="{{ route('show.register') }}">Register</a>
     <a href="">Logout</a>
 </nav>
+
+<link rel="stylesheet" href="{{ asset('css/header.css') }}">
+<div class="main-header">
+    <div class="logo" href="{{ route('homepage') }}">KayaGov</div>
+    <nav class="nav-links">
+        <a href="#">Logout</a>
+    </nav>
+</div>

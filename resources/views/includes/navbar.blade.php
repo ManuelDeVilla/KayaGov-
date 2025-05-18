977422a835b6b7bc651dfc834c47ffba39497084
Anna <dagosgc@students.nu-laguna.edu.ph>
2025-05-18 22:41:13 +0800

    Remove logout link from navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -2,5 +2,4 @@
     <a href="{{ route('homepage') }}">Home</a>
     <a href="{{ route('show.login') }}">Login</a>
     <a href="{{ route('show.register') }}">Register</a>
-    <a href="">Logout</a>
 </nav>

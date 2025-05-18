c043e726055d9c51277974a897d08ba47c90d1f7
Anna <dagosgc@students.nu-laguna.edu.ph>
2025-05-18 23:02:16 +0800

    Refactor navbar to conditionally display user-specific links

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,5 +1,14 @@
 <nav>
     <a href="{{ route('homepage') }}">Home</a>
-    <a href="{{ route('show.login') }}">Login</a>
-    <a href="{{ route('show.register') }}">Register</a>
+    @auth
+        @if(Auth::user()->usertype === 'staff')
+            <a href="{{ route('dashboard.staff') }}">Staff Dashboard</a>
+        @elseif(Auth::user()->usertype === 'citizen')
+            <a href="{{ route('dashboard.citizen') }}">Citizen Dashboard</a>
+        @endif
+        <a href="{{ route('logout') }}">Logout</a>
+    @else
+        <a href="{{ route('show.login') }}">Login</a>
+        <a href="{{ route('show.register') }}">Register</a>
+    @endauth
 </nav>

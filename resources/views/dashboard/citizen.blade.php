83f70651171e58dd6768b971a9223a6c14b82a4c
Anna <dagosgc@students.nu-laguna.edu.ph>
2025-05-18 23:01:50 +0800

    Add citizen dashboard view with fake user data

---
+++ resources/views/dashboard/citizen.blade.php
@@ -0,0 +1,17 @@
+@php
+    // Fake user for testing
+    $user = (object)[
+        'username' => 'Anna',
+        'usertype' => 'citizen'
+    ];
+@endphp
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>Citizen Dashboard</h2>
+
+</div>
+@endsection
+

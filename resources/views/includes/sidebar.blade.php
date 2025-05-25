f75dfe7e600cef8ca14bd781773fa3c1e3808290
palenciafa <palenciafa@students.nu-laguna.edu.ph>
2025-05-25 19:37:11 +0800

    added sidebar component with navigation links

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,75 @@
+   
+    <aside class="sidebar">
+            <div class="sidebar-section">
+                <div class="logo">
+                <a href="{{ route('homepage') }}">
+                    <img src="{{ asset('images/kayagov_logo.png') }}" alt="KayaGov Logo">
+                </a>
+            </div>
+                <h3>Main</h3>
+                <ul class="nav-menu">
+                    <li class="nav-item">
+                        <a href="{{ route('homepage') }}" class="nav-link">
+                            <i class="fas fa-chart-line"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('citizens.concerns.index')}}" class="nav-link">
+                            <i class="fas fa-exclamation-circle"></i>
+                            <span>Concerns</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('citizens.concerns.create')}}" class="nav-link">
+                            <i class="fas fa-plus-circle"></i>
+                            <span>Create Concern</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="fas fa-list"></i>
+                            <span>My Concerns</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="sidebar-section">
+            <h3>Account</h3>
+            <ul class="nav-menu">
+                @guest
+                    <li class="nav-item">
+                        <a href="{{ route('show.login') }}" class="nav-link">
+                            <i class="fas fa-sign-in-alt"></i>
+                            <span>Login</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('show.register') }}" class="nav-link">
+                            <i class="fas fa-user-plus"></i>
+                            <span>Register</span>
+                        </a>
+                    </li>
+                @endguest
+
+                @auth
+                <li class="nav-item">
+                        <a href="{{ route('user-profile') }}" class="nav-link">
+                            <i class="fas fa-user"></i>
+                            <span>User Profile</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <form action="{{ route('logout') }}" method="POST" style="margin: 0;">
+                        @csrf
+                        <button type="submit" class="nav-link logout-button">
+                            <i class="fas fa-sign-out-alt"></i>
+                            <span>Logout</span>
+                        </button>
+                    </form>
+                    </li>
+                @endauth
+            </ul>
+        </div>
+    </aside>
